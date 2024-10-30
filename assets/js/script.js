442d75d430e039f08de94427ba970a6a44fc5180
EmanPrasojo <ecode17@icloud.com>
2024-10-30 13:44:27 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,10 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1200,
+        once: true
+    });
+});
